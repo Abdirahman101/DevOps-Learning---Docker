d0c90876403d6075861f26a5badaa7e9ff60149b
Abdirahman101 <abdirahman.ahmed091@gmail.com>
2025-07-08 17:35:51 +0100

    latest web app files 2

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,11 +1,13 @@
+# app.py
+
 from flask import Flask
 import MySQLdb
-# This is a simple Flask application that returns "Hello, world!" when accessed at the root URL.
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-     # Connect to the MySQL database
+    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="mydb",    # Hostname of the MySQL container
         user="root",    # Username to connect to MySQL
@@ -17,6 +19,5 @@ def hello_world():
     version = cur.fetchone()
     return f'Hello, World! MySQL version: {version[0]}'
 
-if  __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5004)
-# This code defines a Flask application that connects to a MySQL database and retrieves the MySQL version.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
