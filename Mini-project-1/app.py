3e9ad1f1166c65b4c37cf24b74872d694a21f55a
Abdirahman101 <abdirahman.ahmed091@gmail.com>
2025-07-15 20:12:37 +0100

    CoderCo Challenge files

---
+++ Mini-project-1/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+
+r = redis.Redis(host='redis', port=6379)
+#Created redis connection
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to the CoderCo Challenge'
+
+@app.route('/count')
+def count():
+    count = r.incr('visit_count')
+    return f'This page has been visited {count} times!'
+    # Increment the visit count in Redis
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5003)
