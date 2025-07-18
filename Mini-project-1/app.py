ea79b6ed11fcb96a36373a6d690cd23faa3f7f14
Abdirahman101 <abdirahman.ahmed091@gmail.com>
2025-07-18 17:01:07 +0100

    CoderCo Challenge personalisation 2

--- Mini-project-1/app.py
+++ Mini-project-1/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 import redis
 import os
+
 
 app = Flask(__name__)
 
@@ -12,12 +13,13 @@ r = redis.Redis(host=os.getenv('REDIS_HOST', 'redis'), port=int(os.getenv('REDIS
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to the CoderCo Challenge'
+    #return 'Welcome to the CountTracker app'
+    return render_template('index.html', title='CountTracker App', message='Welcome to the CountTracker App!')
 
 @app.route('/count')
 def count():
     count = r.incr('visit_count')
-    return f'This page has been visited {count} times!'
+    return render_template('index.html', title='Count', message=f'This page has been visited {count} times!')
     # Increment the visit count in Redis
 
 if __name__ == '__main__':
